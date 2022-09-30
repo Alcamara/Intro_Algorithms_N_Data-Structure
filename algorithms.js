7e9a745c68746e0e275e7f8e29cf874a2f82d2a0
Al Camara <Alhousseinec@gmail.com>
2022-09-30 01:43:55 -0500

    added binary search function

--- algorithms.js
+++ algorithms.js
@@ -1,9 +1,9 @@
 /**
  * 
  * Linear Search
+ * 
+ * return index for value
  */
-
-
 
 function linearSearch(array, key){
     for(let i = 0; i < array.length; i++){
@@ -14,6 +14,31 @@ function linearSearch(array, key){
     return -1;
   }
 
+  /**
+   * 
+   * binary Search
+   */
 
-  console.log(linearSearch([1,2,3,4,5,6,7,8,9,10], 19))
+  function binarySearch(array, key) {
+    let left = 0;
+    let right = array.length - 1;
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+        console.log(array[mid]);
+        if (array[mid] === key) {
+            return mid;
+        }
+        if (array[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+
+
+//   console.log(linearSearch([1,2,3,4,5,6,7,8,9,10], 5));
   
+  console.log(binarySearch([1,2,3,4,5,6,7,8,9,10],10));
