a40a95bb1db09bb57d4b5e949d8bceeb4d54c0d5
Al Camara <Alhousseinec@gmail.com>
2022-09-30 01:08:58 -0500

    added linear search algorithms

---
+++ algorithms.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Linear Search
+ */
+
+
+
+function linearSearch(array, key){
+    for(let i = 0; i < array.length; i++){
+      if(array[i] === key) {
+          return i;
+      }
+    }
+    return -1;
+  }
+
+
+  console.log(linearSearch([1,2,3,4,5,6,7,8,9,10], 19))
+  
